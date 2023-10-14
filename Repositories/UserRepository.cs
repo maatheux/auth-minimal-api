e6e12ef386b9ff0f5c301da3e876785c49967cad
Matheus Lima <matheusmarinho.lima@hotmail.com>
2023-10-13 23:35:24 -0300

    Auth and Authorization with Minimal Api

---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,16 @@
+﻿using MinimalApiAuth.Models;
+
+namespace MinimalApiAuth.Repositories;
+
+public static class UserRepository
+{
+    public static User Get(string username, string password)
+    {
+        var users = new List<User>
+        {
+            new User { Id = 1, Username = "batman", Password = "batman", Role = "manager"},
+            new User { Id = 2, Username = "robin", Password = "robin", Role = "empolyee"}
+        };
+        return users.Where(x => x.Username.ToLower() == username.ToLower() && x.Password == password).FirstOrDefault();
+    }
+}
---
+++ Settings.cs
@@ -0,0 +1,6 @@
+﻿namespace MinimalApiAuth;
+
+public static class Settings
+{
+    public static string Secret = "cd9c6ab29b5e4f97913e4f7511ef173d";
+}
